5a785a9542ae14068e397a6b6242daf00d48129a
BenrhayemRacem <racembenrhayem@insat.u-cartage.tn>
2022-10-02 20:20:00 +0100

    exercice 1 du TP1

---
+++ src/app/color-changer/color-changer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-changer',
+  templateUrl: './color-changer.component.html',
+  styleUrls: ['./color-changer.component.css']
+})
+export class ColorChangerComponent implements OnInit {
+  color='transparent'
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getColor(){
+    return this.color
+  }
+  setColor(newColor:string) {
+    this.color = newColor
+  }
+
+}
