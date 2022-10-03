bb307a3d1dfec275535fb5b7cd07c290622df0ac
BenrhayemRacem <racembenrhayem@insat.u-cartage.tn>
2022-10-04 00:55:52 +0100

    exercise 2 TP1 : two-way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColorChangerComponent } from './color-changer/color-changer.component';
-
+import { CarteVisiteComponent } from './carte-visite/carte-visite.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    ColorChangerComponent
+    ColorChangerComponent,
+    CarteVisiteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/carte-visite/carte-visite.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carte-visite',
+  templateUrl: './carte-visite.component.html',
+  styleUrls: ['./carte-visite.component.css']
+})
+export class CarteVisiteComponent implements OnInit {
+
+  firstName:string = ""
+  lastName:string = ""
+  image:string = ""
+  job:string = ""
+  quote:string = ""
+  keyWords:string = ""
+  jobDescription:string=""
+  rotate:boolean=false
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  setRotate(b:boolean) {
+    this.rotate=b
+  }
+
+
+}
